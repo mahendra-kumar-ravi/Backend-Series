ce4d3cf2a35d765adf3ec03435cc44929d922da6
Mahendra <178223873+mahendra-kumar-ravi@users.noreply.github.com>
2025-06-14 23:59:27 +0530

    This is simple Javascript basic practice

---
+++ script.js
@@ -0,0 +1,51 @@
+// Fundamental of JavaScript
+//  Array and Object
+// Function return 
+// async is coading 
+// foreach map filter find indexOf
+
+/*
+
+var arr = [1, 2, 3, 4, 5]; // array can hold multiple type of values 
+// foreach map filter find indexOf
+
+arr.forEach(function(val){
+    // this will not change the original array
+    console.log(val +" Hello"); // 2, 4, 6, 8, 10
+
+})
+
+
+
+// map 
+
+var arr = [1, 2, 3, 4, 5]; 
+var ans = arr.map(function(val){
+    console.log(val);
+    console.log(val + 20);
+    return val;
+})
+
+console.log(ans);
+
+
+// filter 
+var arr = [1, 2, 3, 4, 5];
+var fil = arr.filter(function(val){
+    return val>2  // its comparision which is returning true or false 
+})
+console.log(fil)
+
+// find
+var arr = [1,2,3,4,5,6]
+var fin = arr.find(function(val){
+    return val ==3
+})
+console.log(fin)
+
+
+// indexOf
+var arr = [1,2,3,4,5]
+arr.indexOf(3)  // This will work on browser 
+
+*/
